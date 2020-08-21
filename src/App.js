b543a4dc70cb3d5e1dd71b52a964274af074fb5d
Fuad9 <alfuad8548@gmail.com>
2020-08-21 16:53:00 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const Person = {
+    name: "david",
+    age: 30,
+  };
+
+  const personStyle = {
+    color: "blue",
+    backgroundColor: "yellow",
+  };
+
+  const Subscriber = ["reaganFoxx", "arnold", "megan"];
   return (
     <div className="App">
       <header className="App-header">
@@ -10,17 +21,29 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p style={personStyle}>
+          Howdy! I am {Person.name}, I am {Person.age} years old
+        </p>
+        <User name="Roger" age="40"></User>
+        <User name={Subscriber[0]}></User>
       </header>
     </div>
   );
 }
+
+const User = (props) => {
+  const userStyle = {
+    color: "green",
+    border: "2px solid red",
+    borderRadius: "10px",
+    margin: "4px",
+  };
+  return (
+    <div style={userStyle}>
+      <h1>Name: {props.name}</h1>
+      <h2>Age: {props.age}</h2>
+    </div>
+  );
+};
 
 export default App;
